5de746d16ba68ad60ac68cf67584e885289263d9
ayu0223 <gintokids@gmail.com>
2020-06-13 02:19:41 +0900

    顔認証番

--- main.py
+++ main.py
@@ -52,3 +52,60 @@ def handle_message(event):
 if __name__ == "__main__":
     port = int(os.getenv("PORT"))
     app.run(host="0.0.0.0", port=port)
+
+# 画像メッセージの場合
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_image(event):
+    message_id = event.message.id
+    message_content = line_bot_api.get_message_content(message_id)
+
+    image_bin = BytesIO(message_content.content)
+    image = image_bin.getvalue()
+
+    # S3へ画像を保存
+    s3 = boto3.client('s3')
+    filename = message_id + '.jpg' # メッセージIDをファイル名とする
+    s3.put_object(Bucket='backetを指定', Body=image,  Key=filename)
+
+ # Rekognition呼び出し
+    rekognition = boto3.client('rekognition')
+    response = rekognition.detect_faces(
+        Image={
+            'S3Object': {
+                'Bucket': 'backetを指定',
+                'Name': filename
+            }
+        },
+        Attributes=[
+            'ALL'
+        ]
+    )
+
+    # 結果から感情データを取得
+    message = ''
+    cnt = 0
+    for face_detail in response['FaceDetails']:
+        cnt += 1
+        message += str(cnt) + '人目\n'
+        for emotion in face_detail['Emotions']:
+            message += translation(emotion['Type']) + ':' + \
+                str(round(emotion['Confidence'], 6)) + '\n'
+
+
+def translation(type):
+    if type == 'HAPPY':
+        return '幸せ'
+    elif type == 'SAD':
+        return '悲しみ'
+    elif type == 'ANGRY':
+        return '怒り'
+    elif type == 'SURPRISED':
+        return '驚き'
+    elif type == 'DISGUSTED':
+        return '嫌悪'
+    elif type == 'CALM':
+        return '穏やか'
+    elif type == 'CONFUSED':
+        return '困惑'
+    elif type == 'FEAR':
+        return '恐れ'
